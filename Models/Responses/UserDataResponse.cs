e78bb3648ff8846bd846524477da3058b043fd3c
higorbandeira <34255323+higorbandeira@users.noreply.github.com>
2020-09-03 23:05:49 -0300

    first

---
+++ Models/Responses/UserDataResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SeedingPrecision.Models.Responses
+{
+    public class UserDataResponse
+    {
+        public string Name { get; set; }
+
+        public string Farm { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
