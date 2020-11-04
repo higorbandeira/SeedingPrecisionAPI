0ae6083aa6c06a5b0f95aaf5d9db84e6b638f29f
Johnny <johnny.tcf@gmail.com>
2020-11-04 15:17:02 -0300

    reajuste

--- Controllers/DataController.cs
+++ Controllers/DataController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 using SeedingPrecision.Models.Responses;
 using SeedingPrecision.Service;
@@ -13,6 +14,7 @@ namespace SeedingPrecision.Controllers
     [Route("api/")]
     public class DataController : Controller
     {
+        private readonly IConfiguration _configuration;
         // GET api/user/userdata
         //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [AllowAnonymous]
@@ -24,12 +26,34 @@ namespace SeedingPrecision.Controllers
             return Ok(result);
         }
 
-        //[HttpGet("listStatusHistory/{NumberOfTable}")]
-        //public async Task<ActionResult<List<StatusAtualResponse>>> ListStatusHistory(string NumberOfTable)
-        //{
-        //    HistoryStatusService hss = new HistoryStatusService();
-        //    var result = await hss.GetHistoryStatus(NumberOfTable);
-        //    return Ok(result);
-        //}
+        [HttpGet("listStatusHistory/{NumberOfTable}")]
+        public async Task<ActionResult<List<StatusAtualResponse>>> ListStatusHistory(string NumberOfTable)
+        {
+            HistoryStatusService hss = new HistoryStatusService(_configuration, NumberOfTable);
+            var result = await hss.AjusteHistorys();
+            return Ok(result);
+        }
+        [HttpGet("listStatusPerSensor/{NumberOfTable,sensor}")]
+        public async Task<ActionResult<List<SensorModel>>> ListStatusPerSensor(string NumberOfTable, string sensor)
+        {
+            HistoryStatusService hss = new HistoryStatusService(_configuration, NumberOfTable);
+            var result = await hss.TakeHistorysBySensor(sensor);
+            return Ok(result);
+        }
+
+        [HttpGet("listAllStatusPerSensor/{NumberOfTable}")]
+        public async Task<ActionResult<List<List<SensorModel>>>> ListAllStatusPerSensor(string NumberOfTable)
+        {
+            List<IEnumerable<SensorModel>> result = new List<IEnumerable<SensorModel>> { };
+            string[] Sensores = { "pH", "luminosidade", "tempSolo", "tempAmbiente", "humidSolo", "humidAmbiente" };
+            HistoryStatusService hss = new HistoryStatusService(_configuration, NumberOfTable);
+
+            foreach(string sensor in Sensores)
+            {
+                var aux = await hss.TakeHistorysBySensor(sensor);
+                result.Add(aux);
+            }
+            return Ok(result);
+        }
     }
 }
