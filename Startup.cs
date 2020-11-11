81f3f4413d4e0cee1cc30e22e44eaf64e3ec73f3
higor.bandeira <higor.bandeira@oransys.net>
2020-11-11 09:40:54 -0300

    wip

--- Startup.cs
+++ Startup.cs
@@ -69,6 +69,7 @@ namespace SeedingPrecision
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(s => s.AddDefaultPolicy(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
